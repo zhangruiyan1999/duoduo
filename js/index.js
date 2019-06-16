d1a066dba2158100204dfe619a878197cd18eeb8
Your Name <you@example.com>
2019-06-16 15:01:50 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('#myCarousel').carousel({
+    interval: 2500
+});
+$(".link_image01").mouseover(function(){
+	$(this).css("background-image","url(img/QQ(1).png");
+})
+$(".link_image02").mouseover(function(){
+	$(this).css("background-image","url(img/微博(1).png");
+})
+$(".link_image03").mouseover(function(){
+	$(this).css("background-image","url(img/微信(1).png");
+})
+	
+})
